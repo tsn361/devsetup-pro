f6a6bef8b58ec7c121fe27e90784c13175e0f7eb
Shakhawat hossain <tsn361@gmail.com>
2025-12-24 19:01:33 +0600

    chore: Final release preparation (UI fixes, icon, docs)

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Installing from './pages/Installing';
 import Profiles from './pages/Profiles';
